c0823eb01516689aa12f9fdef95314e91c9225f9
hristomir.dimitrov <hristomir.dimitrov@dxc.com>
2023-07-20 14:45:33 +0300

    new things for Vnet,subnet,peering

--- FG/terraform-project/main.tf
+++ FG/terraform-project/main.tf
@@ -1,12 +1,17 @@
-# main.tf: This file will contain the main configuration for your Terraform project. 
-# You'll define the necessary resources, data sources, and other configurations here.
-# main.tf
-terraform {
-  required_version = ">= 0.12"
-}
+  terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.0"
+    }
+  }
 
-locals {
-  environment = var.environment
+  backend "azurerm" {
+    resource_group_name  = "wonen-prd-h-sa-hub-rg-01"
+    storage_account_name = "wonenprdhsahubst01"
+    container_name       = "wonen-prd-h-sa-hub-state-01"
+    key                  = "policy_initiative.tfstate"
+  }
 }
 
 provider "azurerm" {
@@ -14,40 +19,58 @@ provider "azurerm" {
   # Add any other provider settings here if needed
 }
 
-module "vnet" {
-  source      = "./modules/module1"
-  location    = var.location
-  environment = var.environment
+# Define the resource group
+resource "azurerm_resource_group" "example_rg" {
+  name     = "example-resource-group"
+  location = "East US"  # Replace with your preferred Azure region
 }
 
-module "subnet" {
-  source      = "./modules/module2"
-  location    = var.location
-  environment = var.environment
+# Define the storage account
+resource "azurerm_storage_account" "example_storage" {
+  name                     = "ickoexamplestorage"  # Replace with your preferred storage account name
+  resource_group_name      = azurerm_resource_group.example_rg.name
+  location                 = azurerm_resource_group.example_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
+# Define the virtual network
+resource "azurerm_virtual_network" "example_vnet" {
+  name                = "example-vnet"
+  resource_group_name = azurerm_resource_group.example_rg.name
+  location            = azurerm_resource_group.example_rg.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+# Define the subnet within the virtual network
+resource "azurerm_subnet" "example_subnet" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.example_rg.name
+  virtual_network_name = azurerm_virtual_network.example_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]  # Replace with your preferred subnet CIDR block
+}
+
+module "module1" {
+  source = "./modules/module_subnet"
+
+  # Add your module1-specific variables here, if any
+}
+
+module "module2" {
+  source = "./modules/module_Vnet"
+
+  # Add your module2-specific variables here, if any
+}
+
+# Module for VNet Peering
 module "vnet_peering" {
-  source      = "./modules/module3"
-  environment = var.environment
-}
+  source = "./modules/module_peering"
 
-provider "azurerm" {
-  features {}
-}
+  # Reference to the first VNet module
+  vnet1_name = module.module1.vnet_name
+  vnet1_rg   = module.module1.resource_group_name
 
-# Set the Azure subscription ID using the variable defined in variables.tf
-provider "azurerm" {
-  features {}
-  subscription_id = var.azure_subscription_id
-}
-
-# Call policy and role configurations
-module "policies" {
-  source = "./policies"
-  # If you have additional input variables for the policies module, pass them here.
-}
-
-module "roles" {
-  source = "./roles"
-  # If you have additional input variables for the roles module, pass them here.
+  # Reference to the second VNet module
+  vnet2_name = module.module2.vnet_name
+  vnet2_rg   = module.module2.resource_group_name
 }
--- FG/terraform-project/modules.tf
+++ FG/terraform-project/modules.tf
@@ -21,3 +21,8 @@ module "policy_initiative2" {
 }
 
 # Add other policy initiatives, policy assignments, and other resources as needed.
+
+"appId": "67109c4b-54f1-4203-aa93-e86f964cfdfc",
+  "displayName": "terraform_spn",
+  "password": "XCz8Q~9TAlcqlQIS8C~Ly9eAAubrKCEtNKH4nb_N",
+  "tenant": "93f33571-550f-43cf-b09f-cd331338d086"
